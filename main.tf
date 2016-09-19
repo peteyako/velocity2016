8e97565c5e5897b69c1ba07907887647dd316df4
HashiCorp Training <training@hashicorp.com>
2016-09-19 20:29:38 +0000

    garpage

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ variable "aws_secret_key" {}
 variable "aws_region" {
 	default = "us-east-1"
 }
-
+sdfadfaf
 provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
